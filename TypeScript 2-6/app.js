a8e2a089bfeac121107a1204ca01fea4f9c61287
Nenad <nenojelovac@gmail.com>
2017-02-06 12:07:47 +0100

    poslednKomit

--- TypeScript 2-6/app.js
+++ TypeScript 2-6/app.js
@@ -1,1 +1,9 @@
 var sayHi = function () { console.log('HI!'); };
+var Recept = (function () {
+    function Recept(name, source, ingredients) {
+        this.name = name;
+        this.source = source;
+        this.ingredients = ingredients;
+    }
+    return Recept;
+}());
--- TypeScript 2-6/app.ts
+++ TypeScript 2-6/app.ts
@@ -1,2 +1,18 @@
-let sayHin = () => {console.log('HI!')}
+let sayHio = () => { console.log('HI!') }
 
+
+
+
+interface IIngredients {
+    jajca?: number;
+    brasno: number;
+    mleko: number;
+    kiselomleko: number;
+}
+
+class Recept {
+
+    constructor(public name: string, public source: string, public ingredients: IIngredients) {
+
+    }
+}
