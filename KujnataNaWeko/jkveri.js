599c8b0329839a836cea46f3b4a33e0876f3f814
Nenad <nenojelovac@gmail.com>
2017-02-10 02:01:32 +0100

    reseno listanje

--- KujnataNaWeko/jkveri.js
+++ KujnataNaWeko/jkveri.js
@@ -6,22 +6,54 @@ $(() => {
     let $btnAddNewIngr = $("#newIngredient");
     let $inputIngredients = $("#ingredients");
 
-    let $ddlSostojki = $("#opcii")
+    let $ddlSostojki = $("#opcii");
+    let $inputKolicina = $("#qty");
 
-    // Od tuka nadolu stavame liseneri
-    $btnAddNewIngr.on('click', () => {
-        $inputIngredients.append(
-            `<li>Filip</li>`
+    let count = 0;
+
+    allIngridients.forEach((sostojka, index) => {
+        $ddlSostojki.append(
+            `<option value='${sostojka.toLowerCase()}'>${sostojka}</option>`
         )
     })
 
-    $ddlSostojki.on('click', () => {
+
+
+
+    // Od tuka nadolu stavame liseneri
+    $btnAddNewIngr.on('click', () => {
+        count++;
+        $inputIngredients.append(
+            `<li><span>${$ddlSostojki.val()}</span>, kolicina ${$inputKolicina.val()}</li>`
+        )
+
         $ddlSostojki.html('');
+        $inputKolicina.html('');
+
         allIngridients.forEach((sostojka, index) => {
-            $ddlSostojki.append(
-                `<option value='${sostojka.toLowerCase()}'>${sostojka}</option>`
-            )
+            debugger;
+            if (!~($('li span').text()).indexOf(sostojka.toLowerCase())) {
+                $ddlSostojki.append(
+                    `<option value='${sostojka.toLowerCase()}'>${sostojka}</option>`
+                )
+            }
         })
 
+        if (count === allIngridients.length) {
+            $ddlSostojki.hide();
+            $inputKolicina.hide();
+            $btnAddNewIngr.hide();
+        }
     })
+
+    // $ddlSostojki.on('click', () => {
+    //     $ddlSostojki.html('');
+
+    //     allIngridients.forEach((sostojka, index) => {
+    //         $ddlSostojki.append(
+    //             `<option value='${sostojka.toLowerCase()}'>${sostojka}</option>`
+    //         )
+    //     })
+
+    // })
 })
